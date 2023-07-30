d272d458eb180c2952e98b90d34ed00d7efa42f0
DHANSHRI CHORGHE <102779957+DHANSHRICHORGHE19@users.noreply.github.com>
2023-07-30 20:06:52 +0530

    Update number_game.java  Levels are added in th game.

--- number_game.java
+++ number_game.java
@@ -2,23 +2,26 @@
 // Java program for the above approach
 import java.util.Scanner;
 
-public class number_game {
+public class Demo {
 
 	// Function that implements the
 	// number guessing game
-	public static void guessNumber()
+	public static void guessNumber1()
 	{
 		// Scanner Class
+		System.out.println("LEVEL 1: ");
+		// System.out.println("Guess number n 5 trials:");
+		// System.out.println("Enter a no between 1 to 50 to guess: ");
 		Scanner sc = new Scanner(System.in);
-
+		
 		// Generate the numbers
-		int number = 1 + (int)(100* Math.random());
+		int number = 1 + (int)(51* Math.random());
 
 		int K = 5;  // Given K trials
 
 		int i, guess;
 
-		System.out.println("A number is chosen between 1 to 100.Guess the number within 5 trials.");
+		System.out.println("A number is chosen between 1 to 50.Guess the number within 5 trials.");
 
 		// Iterate over K Trials
 		for (i = 0; i < K; i++) {
@@ -33,7 +36,8 @@ public class number_game {
 			if (number == guess) {
 				System.out.println(
 					"Congratulations!"
-					+ " You guessed the number.");
+					+ " You guessed the number. You won Level 1!!");
+					guessNumber2();
 				break;
 			}
 			else if (number > guess
@@ -52,18 +56,131 @@ public class number_game {
 
 		if (i == K) {
 			System.out.println(
-				"You have exhausted"
-				+ " K trials.");
+				"You have exhausted your trials.");
 
 			System.out.println(
 				"The number was " + number);
 		}
+		
+	}
+	public static void guessNumber2()
+	{
+		// Scanner Class
+		System.out.println("LEVEL 2: ");
+		System.out.println("You entered in second level=>");
+		// System.out.println("Enter a no between 1 to 50 to guess: ");
+		Scanner sc = new Scanner(System.in);
+		int min = 50;  
+     	int max = 101; 
+		// Generate the numbers
+		int number = 1 + (int)(Math.random()*(max-min+1)+min);
+
+		int K = 5;  // Given K trials
+
+		int i, guess;
+
+		System.out.println("A number is chosen between 50 to 100.Guess the number within 5 trials.");
+
+		// Iterate over K Trials
+		for (i = 0; i < K; i++) {
+
+			System.out.println(
+				"Guess the number: ");
+
+			// Take input for guessing
+			guess = sc.nextInt();
+
+			// If the number is guessed
+			if (number == guess) {
+				System.out.println(
+					"Congratulations!"
+					+ " You guessed the number.You won Level 2!!");
+					guessNumber3();
+				break;
+			}
+			else if (number > guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is "
+					+ "greater than " + guess);
+			}
+			else if (number < guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is"
+					+ " less than " + guess);
+			}
+		}
+
+		if (i == K) {
+			System.out.println(
+				"You have exhausted your trials.");
+
+			System.out.println(
+				"The number was " + number);
+		}
+		
 	}
 
+	public static void guessNumber3()
+	{
+		// Scanner Class
+		System.out.println("LEVEL 3: ");
+		System.out.println("You entered in second level=>");
+		// System.out.println("Enter a no between 1 to 50 to guess: ");
+		Scanner sc = new Scanner(System.in);
+		 
+		int number = 1 + (int)(100* Math.random());
+		int K = 5;  // Given K trials
+		 
+		int i, guess;
+
+		System.out.println("A number is chosen between 1 to 100.Guess the number within 5 trials.");
+
+		// Iterate over K Trials
+		for (i = 0; i < K; i++) {
+
+			System.out.println(
+				"Guess the number:");
+
+			// Take input for guessing
+			guess = sc.nextInt();
+
+			// If the number is guessed
+			if (number == guess) {
+				System.out.println(
+					"Congratulations!"
+					+ " You guessed the number. You won Level 3!!");
+					guessNumber2();
+				break;
+			}
+			else if (number > guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is "
+					+ "greater than " + guess);
+			}
+			else if (number < guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is"
+					+ " less than " + guess);
+			}
+		}
+
+		if (i == K) {
+			System.out.println(
+				"You have exhausted your trials.");
+
+			System.out.println(
+				"The number was " + number);
+		}
+		
+	}
 	// Driver Code
 	public static void main(String arg[])
 	{
         // Function Call
-		guessNumber();
+		guessNumber1();
 	}
 }
